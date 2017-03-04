1bbb530a7e5e1ac8fb2091752fe2722a4e3f0357
Toti Cuervo <toticuervo@totis-mbp.memphis.edu>
2017-03-03 20:45:57 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+ Rails.application.routes.draw do
+
+  #Define root URL
+   root 'pages#index'
+
+
+
+   #Define route Route
+  get '/home' => 'pages#home'
+
+  get '/profile' => 'pages#profile'
+
+  get '/explore' => 'pages#explore'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
